edc86b573dcaf680980ee785df0dd6d6d1a115dd
Suyeol Jeon <devxoul@gmail.com>
2016-06-30 00:11:21 +0900

    Start Xcode project

---
+++ RxDelegateCells/RxDelegateCells.h
@@ -0,0 +1,19 @@
+//
+//  RxDelegateCells.h
+//  RxDelegateCells
+//
+//  Created by Suyeol Jeon on 6/30/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxDelegateCells.
+FOUNDATION_EXPORT double RxDelegateCellsVersionNumber;
+
+//! Project version string for RxDelegateCells.
+FOUNDATION_EXPORT const unsigned char RxDelegateCellsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxDelegateCells/PublicHeader.h>
+
+
